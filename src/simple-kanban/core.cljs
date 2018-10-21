bd6b4066603a71e574ef2d508ca81a2392404489
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-10-21 13:32:14 +0300

    Simplified requires

--- src/simple-kanban/core.cljs
+++ src/simple-kanban/core.cljs
@@ -1,36 +1,30 @@
 (ns simple-kanban.core
-  (:require [reagent.core :as r]
-            [simple-kanban.utils :as u :refer [card-data]]
-            [simple-kanban.state :as state :refer [in-backlog
-                                               in-dev
-                                               in-test
-                                               in-done
-                                               new-task-description
-                                               new-task-owner
-                                               id-counter]]))
+  (:require [reagent.core :as reagent]
+            [simple-kanban.utils :as utils]
+            [simple-kanban.state :as state]))
 
 ;; -------------------------
 ;; Actions
 
 (defn reset-inputs []
-  (reset! new-task-description "")
-  (reset! new-task-owner ""))
+  (reset! state/new-task-description "")
+  (reset! state/new-task-owner ""))
 
 (defn remove-card [id cards]
   (swap! cards dissoc id))
 
 (defn advance-card [card cards destination]
-  (if (nil? destination) (remove-card (u/card-data card :id) cards))
-  (let [id (u/card-data card :id)
-        task (u/card-data card :task)
-        completed (u/card-data card :completed)
-        owner (u/card-data card :owner)]
+  (if (nil? destination) (remove-card (utils/card-data card :id) cards))
+  (let [id (utils/card-data card :id)
+        task (utils/card-data card :task)
+        completed (utils/card-data card :completed)
+        owner (utils/card-data card :owner)]
     (swap! destination assoc id {:id id :task task :completed completed :owner owner}))
-  (remove-card (u/card-data card :id) cards))
+  (remove-card (utils/card-data card :id) cards))
 
 (defn add-card-to-backlog [task owner]
   (let [id (swap!
-            id-counter inc)]
+            state/id-counter inc)]
     (swap! state/in-backlog assoc id {:id id :task task :completed false :owner owner}))
   (reset-inputs))
 
@@ -54,7 +48,8 @@
               (= (count @state/new-task-owner) 0))} "Add new task"]])
 
 (defn card-component [card cards button-text advance-to]
-  [:li "Task: " (u/card-data card :task) [:br] " Owner: " (u/card-data card :owner)
+  ^{:key card}
+  [:li "Task: " (utils/card-data card :task) [:br] " Owner: " (utils/card-data card :owner)
    [:br]
    [:button {:class "advance-card-button" :on-click #(advance-card card cards advance-to)}
     button-text]])
@@ -62,7 +57,7 @@
 (defn card-list-component [cards button-text advance-to]
   [:ul
    (for [card @cards]
-     (card-component ^{:key card} card cards button-text advance-to))])
+     (card-component card cards button-text advance-to))])
 
 (defn backlog-section-component []
   [:div {:class "section"} [:div {:class "column-header"} (str "Backlog ( " (count @state/in-backlog) " )")]
@@ -94,7 +89,7 @@
 ;; Initialize app
 
 (defn mount-root []
-  (r/render [app] (.getElementById js/document "app")))
+  (reagent/render [app] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
