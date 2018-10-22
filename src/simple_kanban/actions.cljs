8d9fb2f458571742273fdbba3f54c0055141bb89
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-10-22 22:52:08 +0300

    Fixed a bug with niö swap

--- src/simple_kanban/actions.cljs
+++ src/simple_kanban/actions.cljs
@@ -10,13 +10,14 @@
   (swap! cards dissoc id))
 
 (defn advance-card [card cards destination]
-  (if (nil? destination) (remove-card (utils/card-data card :id) cards))
-  (let [id (utils/card-data card :id)
-        description (utils/card-data card :description)
-        completed (utils/card-data card :completed)
-        owner (utils/card-data card :owner)]
-    (swap! destination assoc id {:id id :description description :completed completed :owner owner}))
-  (remove-card (utils/card-data card :id) cards))
+  (if (nil? destination)
+    (remove-card (utils/card-data card :id) cards)
+    (let [id (utils/card-data card :id)
+          description (utils/card-data card :description)
+          completed (utils/card-data card :completed)
+          owner (utils/card-data card :owner)]
+      (swap! destination assoc id {:id id :description description :completed completed :owner owner})
+      (remove-card (utils/card-data card :id) cards))))
 
 (defn add-card-to-backlog [description owner]
   (let [id (swap!
