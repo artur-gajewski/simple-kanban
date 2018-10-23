03fcf73d08cd69a5c88c04262ca8372335437799
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-10-23 22:44:49 +0300

    Added conditional styles based on card count in alert enabled section

--- src/simple_kanban/components/section.cljs
+++ src/simple_kanban/components/section.cljs
@@ -2,12 +2,14 @@
   (:require [simple-kanban.components.new-card-input :as new-card-input]
             [simple-kanban.components.card-list :as card-list]))
 
-(defn render [title button-text section-state advance-to is-backlog enable-alert]
+(defn render [title button-text current-section-cards next-section-cards is-backlog enable-alert]
   [:div {:class "section"}
-   [:div {:class "column-header"}
-    (str title " ( " (count @section-state) " )")
-    (when enable-alert
-      (when (> (count @section-state) 3)
-        [:span {:class "alert"} "!!!"]))]
+   (let [cards-in-section (count @current-section-cards)]
+     [:div {:class (if enable-alert
+                     (cond
+                       (< cards-in-section 4) "column-header"
+                       (>= cards-in-section 4) "column-header alert")
+                     "column-header")}
+      (str title " ( " cards-in-section " )")])
    (when is-backlog (new-card-input/render))
-   [card-list/render section-state button-text advance-to]])
+   [card-list/render current-section-cards button-text next-section-cards]])
