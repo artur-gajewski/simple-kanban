3a595c5a98218a2f1bec2fa653ce27df642f0901
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-10-24 19:50:56 +0300

    Removed unneeded task field

--- src/simple_kanban/actions.cljs
+++ src/simple_kanban/actions.cljs
@@ -12,18 +12,16 @@
   (swap! current-section-cards dissoc id))
 
 (defn advance-card [task current-section-cards next-section-cards]
-  (let [{:keys [id description completed owner]} (val task)]
+  (let [{:keys [id description owner]} (val task)]
     (remove-card id current-section-cards)
     (if-not (nil? next-section-cards)
       (swap! next-section-cards assoc id {:id id
                                           :description description
-                                          :completed completed
                                           :owner owner}))))
 
 (defn add-card-to-backlog [description owner]
   (let [id (swap! id-counter inc)]
     (swap! in-backlog assoc id {:id id
                                 :description description
-                                :completed false
                                 :owner owner}))
   (reset-inputs))
