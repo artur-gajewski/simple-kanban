27d7f415a93354f2379a8e671b10124034dcd7a0
Artur Gajewski <artur.gajewski@fraktio.fi>
2018-10-25 08:21:27 +0300

    Added function to style column header

--- src/simple_kanban/components/section.cljs
+++ src/simple_kanban/components/section.cljs
@@ -2,14 +2,20 @@
   (:require [simple-kanban.components.new-card-input :refer [new-card-input]]
             [simple-kanban.components.card-list :refer [card-list]]))
 
+(defn styled-column-header [enable-alert cards-in-section & children]
+  [:div
+   {:class
+    (if enable-alert
+      (cond
+        (< cards-in-section 4) "column-header"
+        (>= cards-in-section 4) "column-header alert")
+      "column-header")}
+   children])
+
 (defn section [title button-text current-section-cards next-section-cards is-backlog enable-alert]
   [:div {:class "section"}
    (let [cards-in-section (count @current-section-cards)]
-     [:div {:class (if enable-alert
-                     (cond
-                       (< cards-in-section 4) "column-header"
-                       (>= cards-in-section 4) "column-header alert")
-                     "column-header")}
+     [styled-column-header enable-alert cards-in-section
       (str title " ( " cards-in-section " )")])
    (when is-backlog (new-card-input))
    [card-list current-section-cards button-text next-section-cards]])
